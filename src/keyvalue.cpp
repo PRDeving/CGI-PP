59fc8e9592928b18fb29d9a49d608983eff4b8ad
PRDeving <pablo.deving@gmail.com>
2017-03-24 22:12:13 +0100

    parse request as key value

--- src/keyvalue.cpp
+++ src/keyvalue.cpp
@@ -5,9 +5,10 @@ void List::split(const std::string &s, char delim, Out result) {
   std::stringstream ss;
   ss.str(s);
   std::string item;
-  while (std::getline(ss, item, delim)) {
-    *(result++) = item;
-  }
+  std::getline(ss, item, delim);
+  *(result) = item;
+  std::getline(ss, item);
+  *(result++) = item;
 }
 
 List::KeyValue List::split(const std::string &s, char delim) {
