dd2941321397b67adae9429cbdedbfba18d58328
PRDeving <pablo.deving@gmail.com>
2017-03-24 23:05:23 +0100

    get queryed path

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include "keyvalue.hpp"
 #include "request.hpp"
 
-void showListAsHTML(List::KeyValue kv) {
+void printList(List::KeyValue kv) {
   std::cout << "<p>" << kv[0] << ": " << kv[1] << "</p>" << std::endl;
 }
 
@@ -14,11 +14,14 @@ int main(int argc, char *argv[], const char* env[]) {
 
   std::cout << "<h1>Request</h1>" << std::endl;
   List::KeyValueList request = Request::parse_request(env);
-  List::each(&request, &showListAsHTML);
+  List::each(&request, &printList);
+
+  std::cout << "<h1>Path</h1>" << std::endl
+    << "<p>" << List::find(&request, "PATH_INFO") << "</p>" << std::endl;
 
   std::cout << "<h1>Data</h1>" << std::endl;
   List::KeyValueList data = Request::parse_data(List::find(&request, "QUERY_STRING"));
-  List::each(&data, &showListAsHTML);
+  List::each(&data, &printList);
 
   return 0;
 }
